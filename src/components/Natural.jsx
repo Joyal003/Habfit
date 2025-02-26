2be115d2ab9160671d3946ddf881fc60ba1805d7
Joyal <joyal.com>
2025-02-26 14:29:50 +0530

    path fix

--- src/components/Natural.jsx
+++ src/components/Natural.jsx
@@ -4,11 +4,11 @@ import Contact from '../Common/Contact'
 import Welcome from './Natural/Welcome'
 import WhyChooseHabfit from './Natural/WhyChooseHabfit'
 import BodybuildingServices from './Natural/BodybuildingServices'
-import BodybuildingBonuses from './Natural/BodyBuildingBonuses'
 import LimitedOffer from './Natural/LimitedOffer'
 import JourneyForm from './Natural/JourneyForm'
 import TransformationSection from './Natural/TransformationSection'
 import FAQ from './Natural/FAQ'
+import BodybuildingBonuses from './Natural/BodybuildingBonuses'
 
 const Natural = () => {
   return (
